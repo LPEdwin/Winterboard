87147437bb6de47a09af3d1cbf3872439a67b572
Edwin <n.baumgaertner@gmail.com>
2025-10-19 03:53:36 +0200

    remove unused import

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import { HDRLoader } from 'three/examples/jsm/loaders/HDRLoader.js';
 
 const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
 
