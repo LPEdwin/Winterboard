4c9bbe218e5e97ca1fe4f5c897f62a03ed50ec42
Edwin <n.baumgaertner@gmail.com>
2025-10-20 01:25:13 +0200

    fix target browser

--- vite.config.ts
+++ vite.config.ts
@@ -1,2 +1,5 @@
 import { defineConfig } from 'vite'
-export default defineConfig({ base: '/Winterboard/' })
+export default defineConfig({ 
+    base: '/Winterboard/',
+    build: { target: 'es2022' },
+})
