d184a451dced9ee566e757a63849d06ecee9237a
Edwin <n.baumgaertner@gmail.com>
2025-10-20 04:35:40 +0200

    cache isMobil value

--- src/device.ts
+++ src/device.ts
@@ -1,8 +1,12 @@
+let _isMobile: boolean | undefined;
+
 export function isMobile(): boolean {
+    if (_isMobile) return _isMobile;
     const ua = navigator.userAgent || '';
     const chMobile = (navigator as any).userAgentData?.mobile === true; // Chrome/Edge
     const touch = navigator.maxTouchPoints > 0 || matchMedia('(pointer: coarse)').matches;
     const iPadOS = /Mac/.test(ua) && touch; // iPad iOS13+ without platform
     const uaMobile = /Android|iPhone|iPad|iPod/i.test(ua);
-    return !!(chMobile || uaMobile || iPadOS);
+    _isMobile = !!(chMobile || uaMobile || iPadOS);
+    return _isMobile;
 }
