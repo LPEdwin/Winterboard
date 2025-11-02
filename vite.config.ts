26756761973964d8aab64ab96dd599fc8f917583
Edwin <n.baumgaertner@gmail.com>
2025-11-02 19:35:26 +0100

    Add compile check plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,11 @@
+
+import checker from 'vite-plugin-checker';
 import { defineConfig } from 'vite'
-export default defineConfig({ 
+
+export default defineConfig({
     base: '/Winterboard/',
     build: { target: 'es2022' },
+    plugins: [
+        checker({ typescript: true })
+    ]
 })
